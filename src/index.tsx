458efc761681286e66735bbca03e4bb8fb00b67e
Bartek Fryzowicz <bartekfr@wp.pl>
2021-05-25 17:23:39 +0200

    Add redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,23 @@ import * as React from 'react'
 import ReactDOM from 'react-dom';
 import { ToastProvider } from 'react-toast-notifications';
 
+import { Provider } from 'react-redux'
+import store from './store/store'
+
 import { ThemeProvider } from 'styled-components'
 import theme from './common/theme'
 import Calendar from './components/calendar/calendarContainer'
 import { GlobalStyles } from './common/styled'
 
 const AppContainer = () => (
-  <ThemeProvider theme={theme}>
-      <ToastProvider>
-        <Calendar />
-      </ToastProvider>
-      <GlobalStyles />
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+        <ToastProvider>
+          <Calendar />
+        </ToastProvider>
+        <GlobalStyles />
+    </ThemeProvider>
+  </Provider>
 )
 
 const rootElement = document.getElementById('root')
