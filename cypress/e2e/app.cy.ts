3d0e894c522c9ff9ae1b07150b6d556aeb8603f8
Bartek F <bartekfr@wp.pl>
2023-03-20 19:14:50 +0100

    Improve/refactor tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,14 +4,14 @@ describe('App', () => {
   it('Loads page', () => {
     cy.visit('/')
 
-    cy.get('#root')
+    cy.get('.fc-view')
   })
 
   it('Can de/select single user', () => {
     cy.get('.users-list').find('button').as('buttons').should('not.be.disabled')
 
     cy.get('@buttons').contains('Mark Novak').as('user').invoke('css', 'background-color').then(userColor => {
-      cy.get('@user').first().click()
+      cy.get('@user').click()
       cy.get('@user').invoke('css', 'background-color').should('be.eq', 'rgb(255, 255, 255)')
 
       cy.get('@user').first().click()
@@ -20,18 +20,27 @@ describe('App', () => {
   })
 
   it('Can de/select all users', () => {
-    cy.get('.users-list').find('button').as('buttons').should('not.be.disabled')
+    cy.get('.users-list').find('button').as('buttons')
+    cy.contains('Select all').as('selectAllBtn')
+    cy.contains('Deselect all').as('deselectAllBtn')
+
     cy.get('@buttons').each($el => {
       cy.wrap($el).invoke('css', 'background-color').should('not.be.eq', 'rgb(255, 255, 255)')
     })
 
-    cy.contains('Deselect all').click()
+    cy.get('@selectAllBtn').should('be.disabled')
+    cy.get('@deselectAllBtn').should('not.be.disabled')
+
+    cy.get('@deselectAllBtn').click()
+
+    cy.get('@selectAllBtn').should('not.be.disabled')
+    cy.get('@deselectAllBtn').should('be.disabled')
 
     cy.get('@buttons').each($el => {
       cy.wrap($el).invoke('css', 'background-color').should('be.eq', 'rgb(255, 255, 255)')
     })
 
-    cy.contains('Select all').click()
+    cy.get('@selectAllBtn').click()
 
     cy.get('@buttons').each($el => {
       cy.wrap($el).invoke('css', 'background-color').should('not.be.eq', 'rgb(255, 255, 255)')
@@ -39,7 +48,6 @@ describe('App', () => {
   })
 
   it('Filters events based on selected users', () => {
-    cy.get('.users-list').find('button').as('buttons').should('not.be.disabled')
     cy.get('.fc-event[data-user-id=1]').as('user1Events')
     cy.get('.fc-event[data-user-id=2]').as('user2Events')
 
@@ -54,5 +62,10 @@ describe('App', () => {
     cy.get('.users-list [data-id=2]').click()
     cy.get('@user2Events')
     cy.get('@user1Events').should('not.exist')
+
+    cy.get('.users-list [data-id=1]').click()
+    cy.get('@user2Events').should('exist')
+    cy.get('@user1Events').should('exist')
   })
+
 })
